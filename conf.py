f92ae69d917c1762d693662d01c0ac17bc0c0edb
Amanuel Eyasu <amanuel.e@ecomplc.com>
2025-01-06 14:06:41 +0300

    last

--- conf.py
+++ conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'sphinxDocumentation'
+project = 'ECom Tehnology P.L.C'
 copyright = '2025, Ecom'
 author = 'Ecom'
 
@@ -18,6 +18,8 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# add a method to use the custom style
+
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -25,3 +27,14 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('css/custom.css')
+
+
+extensions = ['myst_parser']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
